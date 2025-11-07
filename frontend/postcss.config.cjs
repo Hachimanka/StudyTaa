0906c13e5f642a3e611ad7b190717ca759143a78
Hachimanka <leonard.forrosuelo@cit.edu>
2025-11-07 21:20:27 +0800

    update postcss

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,9 +1,6 @@
-// Explicit plugin requires help Vite/PostCSS resolve plugin functions.
 module.exports = {
-  plugins: [
-    // tailwind postcss adapter
-    require('@tailwindcss/postcss'),
-    // add autoprefixer
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
